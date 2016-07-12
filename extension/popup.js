8bb9c91eb0bf7faa1398a6203ab304e6337e3382
Zakaria Ridouh <zridouh@live.com>
2016-07-12 02:05:09 -0700

    format files

--- extension/popup.js
+++ extension/popup.js
@@ -1,9 +1,7 @@
-document.querySelector('#go-to-options').addEventListener(function() {
+document.getElementById('go-to-options').addEventListener(function() {
   if (chrome.runtime.openOptionsPage) {
-    // New way to open options pages, if supported (Chrome 42+).
     chrome.runtime.openOptionsPage();
   } else {
-    // Reasonable fallback.
     window.open(chrome.runtime.getURL('options.html'));
   }
 });
